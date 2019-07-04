1704b9b518d2965f0ec30b4fcc3dced6d0293100
Alex Beregszaszi <alex@rtfs.hu>
2019-07-04 21:58:38 +0100

    Update to newer EVMC

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 use evmc_declare::evmc_declare_vm;
 use evmc_vm::*;
 
-#[evmc_declare_vm("Daytona", "evm")]
+#[evmc_declare_vm("Daytona", "evm", "0.1.0")]
 pub struct Daytona;
 
 impl EvmcVm for Daytona {
@@ -10,12 +10,12 @@ impl EvmcVm for Daytona {
     }
 
     fn execute(&self, code: &[u8], context: &ExecutionContext) -> ExecutionResult {
-        let is_create = context.get_message().kind == evmc_sys::evmc_call_kind::EVMC_CREATE;
+        let is_create = context.get_message().kind() == evmc_sys::evmc_call_kind::EVMC_CREATE;
 
         if is_create {
             ExecutionResult::failure()
         } else {
-            ExecutionResult::success(66, Some(vec![0xc0, 0xff, 0xee]))
+            ExecutionResult::success(66, Some(&[0xc0, 0xff, 0xee]))
         }
     }
 }
